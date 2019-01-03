5bdda3dedaafcfca72227b9f18b531b36335fafa
bartha611 <adambarth611@gmail.com>
2019-01-02 22:49:52 -0600

    completed registration and working on login form.

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
+import sys
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, flash, redirect,  url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -32,30 +33,63 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 class Registration(FlaskForm):
-	username = StringField('username', validators=[InputRequired(), Length(min=10, max=70), Email("must provide a valid email between 10 to 50 characters")], description = "email")
+	username = StringField('username', validators=[InputRequired(), Length(min=10, max=70), Email("must provide a valid email between 10 to 70 characters")])
 	password = PasswordField('Password', validators=[InputRequired(), 
 		EqualTo('confirm', message = "Passwords must match")])
 	confirm = PasswordField('Confirm Password')
 	submit = SubmitField('Submit')
 
+class Login(FlaskForm):
+	username = StringField('username', validators = [InputRequired(), Length(min=10, max=70), Email("must provide a valid email between 10 to 70 characters")])
+	password = PasswordField('Password', validators = [InputRequired()])
+	submit = SubmitField('Submit')
 
 
 @app.route("/")
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 
 @app.route("/register", methods = ["GET", "POST"])
 def register():
-	form = Registration(request.form)
+	form = Registration()
 	if request.method == "POST" and form.validate_on_submit():
 		username = request.form["username"]
 		password = request.form["password"]
 		hashed_password = generate_password_hash(password)
+		check_user_exists = db.execute("SELECT username FROM person WHERE username = :user", {"user":username}).fetchall()
+		if len(check_user_exists) != 0:
+			flash(u'Username already exists!', 'error')
+			print(check_user_exists)
+			return redirect(url_for('register'))
 		db.execute("INSERT INTO person(username, password) VALUES (:username, :password)", {"username": username, "password": hashed_password})
 		db.commit()
-		return "You registered successfully!!!"
+		return redirect(url_for('index'))
 	return render_template('register.html', form = form)
+
+
+@app.route("/login", methods = ["GET","POST"])
+def login():
+	form = Login()
+	if request.method == "POST":
+		username = request.form["username"]
+		password = request.form["password"]
+		hashed_password = generate_password_hash(password)
+		person = db.execute("SELECT :username, :password FROM person", {"username": username, "password": password}).fetchall()
+		db_person = person[0][0]
+		db_password = person[0][1]
+		print(db_password)
+		if len(person) == 0:
+			flash('Username does not exist')
+			return redirect(url_for('login'))
+		else:
+			if username == db_person and check_password_hash(db_password, form.password.data):
+				flash('login successful!!')
+				return redirect(url_for('index'))
+			elif check_password_hash(password, db_password) == False:
+				flash('password has failed')
+				return redirect(url_for('login'))
+	return render_template('login.html', form = form)
 
 
 if __name__ == '__main__':
